56fdb9cd280f8ef374914d3f12d0d3ec744251e3
donsabda <45917800+donsabda@users.noreply.github.com>
2019-11-13 16:03:00 +0700

    Update left-sidebar.php

--- application/views/template/partial/left-sidebar.php
+++ application/views/template/partial/left-sidebar.php
@@ -10,9 +10,6 @@
                     <i class="mdi mdi-view-dashboard"></i><span class="badge badge-primary badge-pill float-right">2</span> <span> Dashboard </span>
                 </a>
             </li>
-            <li>
-                <a href="calendar.html" class="waves-effect"><i class="mdi mdi-calendar-check"></i><span> Calendar </span></a>
-            </li>
 
             <li>
                 <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-email-outline"></i><span> Email <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span></a>
@@ -20,114 +17,6 @@
                     <li><a href="email-inbox.html">Inbox</a></li>
                     <li><a href="email-read.html">Email Read</a></li>
                     <li><a href="email-compose.html">Email Compose</a></li>
-                </ul>
-            </li>
-
-            <li class="menu-title">Components</li>
-
-            <li>
-                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-buffer"></i> <span> UI Elements <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span> </a>
-                <ul class="submenu">
-                    <li><a href="ui-alerts.html">Alerts</a></li>
-                    <li><a href="ui-buttons.html">Buttons</a></li>
-                    <li><a href="ui-badge.html">Badge</a></li>
-                    <li><a href="ui-cards.html">Cards</a></li>
-                    <li><a href="ui-carousel.html">Carousel</a></li>
-                    <li><a href="ui-dropdowns.html">Dropdowns</a></li>
-                    <li><a href="ui-grid.html">Grid</a></li>
-                    <li><a href="ui-images.html">Images</a></li>
-                    <li><a href="ui-lightbox.html">Lightbox</a></li>
-                    <li><a href="ui-modals.html">Modals</a></li>
-                    <li><a href="ui-pagination.html">Pagination</a></li>
-                    <li><a href="ui-popover-tooltips.html">Popover & Tooltips</a></li>
-                    <li><a href="ui-rangeslider.html">Range Slider</a></li>
-                    <li><a href="ui-session-timeout.html">Session Timeout</a></li>
-                    <li><a href="ui-progressbars.html">Progress Bars</a></li>
-                    <li><a href="ui-sweet-alert.html">Sweet-Alert</a></li>
-                    <li><a href="ui-tabs-accordions.html">Tabs & Accordions</a></li>
-                    <li><a href="ui-typography.html">Typography</a></li>
-                    <li><a href="ui-video.html">Video</a></li>
-
-
-                </ul>
-            </li>
-
-            <li>
-                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-clipboard-outline"></i><span> Forms <span class="badge badge-pill badge-success float-right">6</span> </span></a>
-                <ul class="submenu">
-                    <li><a href="form-elements.html">Form Elements</a></li>
-                    <li><a href="form-validation.html">Form Validation</a></li>
-                    <li><a href="form-advanced.html">Form Advanced</a></li>
-                    <li><a href="form-editors.html">Form Editors</a></li>
-                    <li><a href="form-uploads.html">Form File Upload</a></li>
-                    <li><a href="form-xeditable.html">Form Xeditable</a></li>
-
-                </ul>
-            </li>
-
-            <li>
-                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-chart-line"></i><span> Charts <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span></a>
-                <ul class="submenu">
-                    <li><a href="charts-morris.html">Morris Chart</a></li>
-                    <li><a href="charts-chartist.html">Chartist Chart</a></li>
-                    <li><a href="charts-chartjs.html">Chartjs Chart</a></li>
-                    <li><a href="charts-flot.html">Flot Chart</a></li>
-                    <li><a href="charts-c3.html">C3 Chart</a></li>
-                    <li><a href="charts-other.html">Jquery Knob Chart</a></li>
-                </ul>
-            </li>
-
-            <li>
-                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted-type"></i><span> Tables <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span></a>
-                <ul class="submenu">
-                    <li><a href="tables-basic.html">Basic Tables</a></li>
-                    <li><a href="tables-datatable.html">Data Table</a></li>
-                    <li><a href="tables-responsive.html">Responsive Table</a></li>
-                    <li><a href="tables-editable.html">Editable Table</a></li>
-                </ul>
-            </li>
-
-            <li>
-                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-album"></i> <span> Icons <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span></span> </a>
-                <ul class="submenu">
-                    <li><a href="icons-material.html">Material Design</a></li>
-                    <li><a href="icons-ion.html">Ion Icons</a></li>
-                    <li><a href="icons-fontawesome.html">Font Awesome</a></li>
-                    <li><a href="icons-themify.html">Themify Icons</a></li>
-                    <li><a href="icons-dripicons.html">Dripicons</a></li>
-                    <li><a href="icons-typicons.html">Typicons Icons</a></li>
-                </ul>
-            </li>
-
-            <li>
-                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-google-maps"></i><span> Maps <span class="badge badge-pill badge-danger float-right">2</span> </span></a>
-                <ul class="submenu">
-                    <li><a href="maps-google.html"> Google Map</a></li>
-                    <li><a href="maps-vector.html"> Vector Map</a></li>
-                </ul>
-            </li>
-
-            <li class="menu-title">Extras</li>
-
-            <li>
-                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-account-location"></i><span> Authentication <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span></a>
-                <ul class="submenu">
-                    <li><a href="pages-login.html">Login</a></li>
-                    <li><a href="pages-register.html">Register</a></li>
-                    <li><a href="pages-recoverpw.html">Recover Password</a></li>
-                    <li><a href="pages-lock-screen.html">Lock Screen</a></li>
-                </ul>
-            </li>
-
-            <li>
-                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-google-pages"></i><span> Extra Pages <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span></a>
-                <ul class="submenu">
-                    <li><a href="pages-timeline.html">Timeline</a></li>
-                    <li><a href="pages-invoice.html">Invoice</a></li>
-                    <li><a href="pages-directory.html">Directory</a></li>
-                    <li><a href="pages-blank.html">Blank Page</a></li>
-                    <li><a href="pages-404.html">Error 404</a></li>
-                    <li><a href="pages-500.html">Error 500</a></li>
                 </ul>
             </li>
 
