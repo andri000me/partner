e3b4867332594a78a1fd8751515879228c42a884
Ibrahim Ahmad Jabar <30390302+baimtheelite@users.noreply.github.com>
2019-11-19 22:52:38 +0700

    tambah model dan controller maintain

--- application/models/Maintain_model.php
+++ application/models/Maintain_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Mantain_model extends CI_Model
+class Maintain_model extends CI_Model
 {
 
     public function create($data)
