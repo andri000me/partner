ffe64354ab11b51cd8197203a28c0cbafcaaf9a4
Ibrahim Ahmad Jabar <30390302+baimtheelite@users.noreply.github.com>
2020-01-30 14:34:26 +0700

    folder template

--- template/assets/pages/datatables.init.js
+++ template/assets/pages/datatables.init.js
@@ -1,8 +1,48 @@
 $(document).ready(function () {
-	$("#datatable").DataTable({
-		bLengthChange: false
-	}), $("#datatable-buttons").DataTable({
-		lengthChange: !1,
-		buttons: ["copy", "excel", "pdf", "colvis"]
-	}).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)")
+	$("a[data-toggle=\"tab\"]").on("shown.bs.tab", function (e) {
+		$($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+	});
+
+	$(".datatable").DataTable({
+			responsive: true,
+			pagingType: "simple",
+			bLengthChange: false,
+			language: {
+				search: '',
+				searchPlaceholder: "Cari Data Disini"
+			}
+		}),
+		$("#datatable-buttons")
+		.DataTable({
+			responsive: true,
+			pagingType: "simple",
+			lengthChange: !1,
+			language: {
+				search: '',
+				searchPlaceholder: "Cari Data Disini"
+			},
+			buttons: ["pdf", "excel", "colvis"]
+		})
+		.buttons()
+		.container()
+		.appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
+});
+
+$(document).ready(function () {
+	$("a[data-toggle=\"tab\"]").on("shown.bs.tab", function (e) {
+		$($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+	});
+
+	$(".datatable-modal").DataTable({
+		responsive: true,
+		pagingType: "simple",
+		bLengthChange: false,
+		language: {
+			search: '',
+			searchPlaceholder: "Cari Data Disini"
+		},
+		"lengthMenu": [5, 40, 60, 80, 100],
+		"pageLength": 5,
+
+	});
 });
