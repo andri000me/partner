40273c2925acec4e8cb4a119b6b9c3575e18c729
donsabda <45917800+donsabda@users.noreply.github.com>
2020-03-10 08:53:01 +0700

    update datatable button
    upload ulang :
    - assignment-lead-database
    - assignment-lead-prospect
    - datatables.init.js

--- template/assets/pages/datatables.init.js
+++ template/assets/pages/datatables.init.js
@@ -4,28 +4,39 @@ $(document).ready(function () {
 	});
 
 	$(".datatable").DataTable({
-		responsive: true,
-		pagingType: "simple",
-		bLengthChange: false,
+			responsive: true,
+			pagingType: "simple",
+			bLengthChange: false,
+			language: {
+				search: '',
+				searchPlaceholder: "Cari Data Disini"
+			}
+		}),
+		$("#datatable-buttons")
+		.DataTable({
+			responsive: true,
+			pagingType: "simple",
+			lengthChange: false,
+			language: {
+				search: '',
+				searchPlaceholder: "Cari Data Disini"
+			},
+			buttons: ["pdf", "excel", "colvis"]
+		})
+		.buttons()
+		.container()
+		.appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
+
+	$('.datatable-buttons').DataTable({
+		dom: 'Bfrtip',
 		language: {
 			search: '',
 			searchPlaceholder: "Cari Data Disini"
-		}
-	}),
-		$("#datatable-buttons")
-			.DataTable({
-				responsive: true,
-				pagingType: "simple",
-				lengthChange: !1,
-				language: {
-					search: '',
-					searchPlaceholder: "Cari Data Disini"
-				},
-				buttons: ["pdf", "excel", "colvis"]
-			})
-			.buttons()
-			.container()
-			.appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
+		},
+		buttons: [
+			'excel', 'pdf', 'colvis'
+		]
+	});
 });
 
 $(document).ready(function () {
